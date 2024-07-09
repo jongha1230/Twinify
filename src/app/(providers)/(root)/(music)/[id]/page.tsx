81f8918739ef7827edafdb182ddbda67c92280e9
Yejin Choi <llhdf123@naver.com>
2024-07-09 10:33:10 +0900

    240709 [style]: react-icons 설치

--- src/app/(providers)/(root)/(music)/[id]/page.tsx
+++ src/app/(providers)/(root)/(music)/[id]/page.tsx
@@ -1,5 +1,44 @@
+import { FaRegCommentAlt } from "react-icons/fa";
+import { GoHeart } from "react-icons/go";
+import { IoEyeSharp } from "react-icons/io5";
+
 function MusicDetailPage() {
-  return <div>page</div>;
+  return (
+    <main>
+      <article>
+        <section>
+          {/* 앨범 사진, 제목, 가수, 좋아요, 댓글 수, 조회수? */}
+          <div>
+            {/* <Image
+      src={}
+      /> */}
+          </div>
+
+          <div>
+            <h1>제목</h1>
+            <h4>가수</h4>
+            <div>
+              <GoHeart />
+              <span>숫자</span>
+              <FaRegCommentAlt />
+              <span>숫자</span>
+              <IoEyeSharp />
+              <span>숫자</span>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          {/* 가사, 더보기? */}
+          <h2>가사</h2>
+          <p>
+            {/* 더보기 이 안에서, css로 */}
+            내용 가져오기
+          </p>
+        </section>
+      </article>
+    </main>
+  );
 }
 
 export default MusicDetailPage;
