1bae5c30f2ced827047c6ae6df5d4c933384311a
Jongha <jongha1230@gmail.com>
2024-07-09 12:54:59 +0900

    🎉 add: 사이드바 레이아웃 추가

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div className="px-8 py-4">Header</div>;
+}
+
+export default Header;
---
+++ src/components/common/Sidebar/SidebarButton/SidebarButton.tsx
@@ -0,0 +1,61 @@
+import { VariantProps, cva } from "class-variance-authority";
+import Image from "next/image";
+import Link from "next/link";
+
+const sidebarButtonVariants = cva(
+  [
+    "flex",
+    "items-center",
+    "space-x-2",
+    "w-full",
+    "py-1",
+    "transition-colors",
+    "text-base",
+  ],
+  {
+    variants: {
+      active: {
+        true: "bg-brandPrimary font-bold",
+        false: "",
+      },
+      disabled: {
+        true: "opacity-50 cursor-not-allowed",
+        false: "cursor-pointer",
+      },
+    },
+    defaultVariants: {
+      active: false,
+      disabled: false,
+    },
+  }
+);
+
+type SidebarButtonProps = {
+  title: string;
+  href: string;
+  icon?: string;
+  disabled?: boolean;
+} & VariantProps<typeof sidebarButtonVariants>;
+
+function SidebarButton({
+  icon,
+  title,
+  href,
+  disabled = false,
+  active,
+}: SidebarButtonProps) {
+  const buttonClasses = sidebarButtonVariants({ active, disabled });
+
+  return (
+    <Link href={href} className={buttonClasses}>
+      {icon && (
+        <span className="icon flex items-center justify-center">
+          <Image src={icon} alt={`${title} icon`} width={20} height={20} />
+        </span>
+      )}
+      <span>{title}</span>
+    </Link>
+  );
+}
+
+export default SidebarButton;
